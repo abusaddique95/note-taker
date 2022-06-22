5c77f360e5cd6c6ad807281e59a09999c58b3dce
abusaddique95 <abusaddique95@live.com>
2022-06-22 18:10:39 +0100

    lniking to the html

--- src/controllers/views.js
+++ src/controllers/views.js
@@ -1,8 +1,18 @@
+const path = require("path");
+
 const renderHomePage = (req, res) => {
-  res.send("renderHomePage");
+  try {
+    return res.sendFile(path.join(__dirname, "../../utils/index.html"));
+  } catch (error) {
+    console.log(`[error: failed to load homepage |" ${error.message}]`);
+  }
 };
 const renderNotePage = (req, res) => {
-  res.send("renderNotePage");
+  try {
+    return res.sendFile(path.join(__dirname, "../../utils/notes.html"));
+  } catch (error) {
+    console.log(`[error: failed to load notes page |" ${error.message}]`);
+  }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,41 +2,38 @@
 const express = require("express");
 const mysql2 = require("mysql2/promise");
 
-const routes = require("./routes");
-
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+const noteRoutes = require("./routes/notes");
 
 const init = async () => {
-  
   try {
-
     const PORT = process.env.PORT || 4000;
-    
+
     const config = {
-      host: "localhost"
-      user: "root"
-      password: "password"
-      database: "note_taker_db"
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "note_taker_db",
     };
-    
+
     // connect database
     const db = await mysql2.createConnection(config);
-    
+
     const app = express();
+
     // add middleware
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(routes);
-    
+
     // add listener
     app.listen(PORT, () => {
-  
-  console.log(`Sever running on http://localhost:${PORT}`);
-  });
-} catch (error) {
-  console.log(`[ERROR]: failed to start server | ${error.message}`);
-}
+      console.log(`Sever running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: failed to start server | ${error.message}`);
+  }
 };
 
 init();
-
-
