a4cff54523d70e9b6e66870a83bf11763c43d4a4
abusaddique95 <abusaddique95@live.com>
2022-06-22 15:51:56 +0100

    connected database

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 // required frameworks
 const express = require("express");
+const mysql2 = require("mysql2/promise");
 
 const routes = require("./routes");
 
-// choose port
-const PORT = process.env.PORT || 4000;
 
-const app = express();
+const init = async () => {
+  
+  try {
 
-// add middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
-// add listener
-app.listen(PORT, () => {
+    const PORT = process.env.PORT || 4000;
+    
+    const config = {
+      host: "localhost"
+      user: "root"
+      password: "password"
+      database: "note_taker_db"
+    };
+    
+    // connect database
+    const db = await mysql2.createConnection(config);
+    
+    const app = express();
+    // add middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(routes);
+    
+    // add listener
+    app.listen(PORT, () => {
+  
   console.log(`Sever running on http://localhost:${PORT}`);
-});
+  });
+} catch (error) {
+  console.log(`[ERROR]: failed to start server | ${error.message}`);
+}
+};
+
+init();
+
+
