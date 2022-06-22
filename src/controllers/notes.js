05dec606d45a7a16034f373c9ecda3d42396d943
abusaddique95 <abusaddique95@live.com>
2022-06-22 20:33:25 +0100

    create function working

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,11 +1,42 @@
+const { v4: uuidv4 } = require("uuid");
+
+const { readDataFromFile, writeDataToFile } = require("../utils/writetofile");
+
 const getNotes = (req, res) => {
-  res.send("getNotes");
+  try {
+    const db = readDataFromFile("db");
+
+    return res.json(db);
+  } catch (error) {
+    console.log(`[ERROR: Failed to get Notes | ${error.message}]`);
+  }
 };
+
 const deleteNote = (req, res) => {
   res.send("deleteNotes");
 };
+
 const createNote = (req, res) => {
-  res.send("createNotes");
+  const { title, text } = req.body;
+
+  const id = uuidv4();
+
+  const note = {
+    id,
+    title,
+    text,
+  };
+
+  // push
+  const notes = readDataFromFile("db");
+  // new notes
+  notes.push(note);
+
+  writeDataToFile("db", notes);
+
+  return res.json({
+    message: "Successfully created new note",
+  });
 };
 
 module.exports = {
